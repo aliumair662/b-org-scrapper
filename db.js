8506cdb603ce3dbe6614a63383a7d147a925a609
Ali Umair <aliumair662@gmail.com>
2025-07-19 06:22:46 +0500

    edit env

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
-const { MongoClient } = require("mongodb");
-
-const uri = "mongodb://127.0.0.1:27017";
-
+const { MongoClient } = require("mongodb")
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 async function insertData(data) {
